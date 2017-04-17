6a0fc89a67f8c9ae1d0116d3487ccfb1ebcf5aa3
chenyuan <492069026@qq.com>
2017-04-17 11:35:44 +0800

    11

--- jl-vue/js/filter.js
+++
@@ -1,2 +0,0 @@
-/* filter.js Date:2017-04-17 11:22:24 */
-define("filter.js",["vue.js"],function(require,a,b){require("vue.js");var c={filter:function(){Vue.filter("typeAge",function(a){return a}),Vue.filter("typeSex",function(a){var b={female:"\u5973",male:"\u7537"};return b[a]})}};b.exports={filter:c.filter}});
